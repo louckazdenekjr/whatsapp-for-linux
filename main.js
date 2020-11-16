715c02343bd5091c67c8ee0bd01dd450be86e37b
Zdenek Loucka <louckazdenekjr@gmail.com>
2020-11-16 22:40:37 +0100

    quickfix to main.js

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ function createWindow () {
         plugins: false,
     }
   })
-    app.webContents.setUserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36");
+    win.webContents.setUserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36");
     win.loadURL('https://web.whatsapp.com/')  
 }
 
