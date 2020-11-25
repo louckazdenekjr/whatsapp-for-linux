54baedce6f9919c2c68a195193e9a36a19dfba6b
Zdenek Loucka <louckazdenekjr@gmail.com>
2020-11-25 15:27:15 +0100

    default theme = light

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, Menu} = require('electron')
+const { app, BrowserWindow, Menu, nativeTheme} = require('electron')
 
 var path = require("path");
 
@@ -15,6 +15,7 @@ function createWindow () {
         plugins: false,
     }
   })
+	nativeTheme.themeSource = 'light'
     win.webContents.setUserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36");
     win.loadURL('https://web.whatsapp.com/')  
 }
